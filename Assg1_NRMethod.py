9889dae4d469085ad78d89d9a23193738e9ddc0d
MOHAMMAD HUSAIN <36817412+Aurangzeb123@users.noreply.github.com>
2019-08-21 11:03:00 +0530

    Update Assg1_NRMethod.py

--- Assg1_NRMethod.py
+++ Assg1_NRMethod.py
@@ -8,15 +8,14 @@ itn_max = 10
 
 while itn<itn_max:
     if eps <= eps_max:
-        f = x1*x1*x1 + x1 -1
+        f = x1**3 + x1 -1
         print("solution in %d iterations is: %f and function value = %f" %(itn, x1, f))
         if f == 0:
             print("we found f = 0 in %d iterations" %(itn))
             break
     x0 = x1
     itn = itn + 1
-    f0 = x0*x0*x0 + x0 - 1
-    f_dev0 = 3*x0*x0 + 1
+    f0 = x0**3 + x0 - 1
+    f_dev0 = 3*x0**2 + 1
     x1 = x0 - (f0/f_dev0)
     eps = x1 - x0
-    
